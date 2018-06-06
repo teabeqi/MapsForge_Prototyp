f80614a77f585e8bd4419d7edbff236f03d67b40
teabeqi <36037088+teabeqi@users.noreply.github.com>
2018-06-06 11:20:13 +0200

    Fünftes Upload
    Fertige Darstellung der Karte und Beginn mit Location Services (GPS Tracking)

--- src/main/java/com/example/user/mapsforge/MainActivity.java
+++ src/main/java/com/example/user/mapsforge/MainActivity.java
@@ -7,20 +7,28 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import org.mapsforge.core.graphics.Color;
+import org.mapsforge.core.graphics.Paint;
+import org.mapsforge.core.graphics.Style;
 import org.mapsforge.core.model.LatLong;
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
 import org.mapsforge.map.android.util.AndroidUtil;
 import org.mapsforge.map.android.view.MapView;
 import org.mapsforge.map.datastore.MapDataStore;
 import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.overlay.Polygon;
+import org.mapsforge.map.layer.overlay.Polyline;
 import org.mapsforge.map.layer.renderer.TileRendererLayer;
 import org.mapsforge.map.reader.MapFile;
 import org.mapsforge.map.rendertheme.InternalRenderTheme;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
     // name of the map file in the external storage
@@ -64,6 +72,48 @@ public class MainActivity extends Activity {
         //Koords of Theth
         this.mapView.setCenter(new LatLong(42.39561, 19.77352));
         this.mapView.setZoomLevel((byte) 17);
+
+        drawPath();
+
+    }
+    // Route erstellen
+    private void drawPath() {
+        LatLong latLong1 = new LatLong(42.39561, 19.77352);
+        LatLong latLong2 = new LatLong(42.39632, 19.77311);
+        LatLong latLong3 = new LatLong(42.39762, 19.77208);
+        LatLong latLong4 = new LatLong(42.39795, 19.77187);
+        LatLong latLong5 = new LatLong(42.39816, 19.77319);
+        // Route zeichnen
+        Paint paint = Utils.createPaint(
+                AndroidGraphicFactory.INSTANCE.createColor(Color.BLUE),
+                (int) (8 * mapView.getModel().displayModel.getScaleFactor()),
+                Style.STROKE);
+
+        Polyline polyline = new Polyline(paint, AndroidGraphicFactory.INSTANCE);
+        // eine List mitt Longitudes erstellen
+        List<LatLong> latLongs = polyline.getLatLongs();
+        latLongs.add(latLong1);
+        latLongs.add(latLong2);
+        latLongs.add(latLong3);
+        latLongs.add(latLong4);
+        latLongs.add(latLong5);
+
+
+        Paint paintFill2 = Utils.createPaint(
+                AndroidGraphicFactory.INSTANCE.createColor(Color.GREEN), 2,
+                Style.FILL);
+        paintFill2.setBitmapShader(AndroidGraphicFactory.convertToBitmap(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? getDrawable(R.drawable.green_marker) : getResources().getDrawable(R.drawable.green_marker)));
+
+        Paint paintStroke2 = Utils.createPaint(
+                AndroidGraphicFactory.INSTANCE.createColor(Color.BLACK), 2,
+                Style.STROKE);
+        Polygon polygonWithShaderNonAligned = new Polygon(paintFill2, paintStroke2,
+                AndroidGraphicFactory.INSTANCE);
+        List<LatLong> latLongs4 = polygonWithShaderNonAligned.getLatLongs();
+        latLongs4.add(latLong1);
+        latLongs4.add(latLong2);
+        latLongs4.add(latLong5);
+
 
     }
 
